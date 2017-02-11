6b4d7df64c566eb5bb0364af218b368b4879346d
Carlos Gómez <carliros.g@gmail.com>
2017-02-10 23:39:01 -0400

    Add support for command options.

--- src/Main.hs
+++ src/Main.hs
@@ -3,20 +3,47 @@ import           Ast
 import           CodeGen
 import           Data.List
 import           Numeric
+import           Options.Applicative
 import           Parser
 import           System.Environment
 import           System.FilePath
 import           System.IO
 
+data FAvrOptions
+  = FAvrOptions { output :: Maybe String
+                , source :: String
+                , device :: Device
+                }
+
+pFAvr :: Parser FAvrOptions
+pFAvr = FAvrOptions
+  <$> optional (strOption (long "output" <> short 'o' <> metavar "FILE" <> help "Write output to FILE"))
+  <*> strArgument (metavar "SOURCE")
+  <*> option deviceReader (long "device" <> short 'd' <> value ATMega328P <> metavar "DEVICE" <> help "Specify the Arduino Device")
+
+deviceReader ::ReadM Device
+deviceReader
+  = eitherReader $ \arg -> if arg == "ATMega328P"
+                           then Right ATMega328P
+                           else Left ("Can not parse '" ++ arg ++ "'")
+
+
+processOptions :: FAvrOptions -> IO ()
+processOptions (FAvrOptions Nothing src device)
+  = compileFAvr (replaceExtension src "asm") src device
+processOptions (FAvrOptions (Just output) src device)
+  = compileFAvr output src device
+
+compileFAvr :: String -> String -> Device -> IO()
+compileFAvr output src device
+  = do result <- parseFile src
+       let genCodeList = sem_Root (Root result) device
+           genCodeString = unlines genCodeList
+       writeFile output genCodeString
+
 main :: IO ()
-main = do
-  putStrLn "Functional AVR Compiler"
-  args <- getArgs
-  if null args
-    then print "No file to compile."
-    else do let file = head args
-            result <- parseFile file
-            let genCodeList = sem_Root (Root result)
-                genCodeString = unlines genCodeList
-                newFileName = replaceExtension file "asm"
-            writeFile newFileName genCodeString
+main = execParser opts >>= processOptions
+  where
+    opts = info (helper <*> pFAvr)
+                (fullDesc <> progDesc "Generate assembly code for AVR"
+                          <> header "Functional AVR Compiler")
